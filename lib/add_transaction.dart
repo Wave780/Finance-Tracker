0d37d7aebd4b1734566caceaba2700669119bfbb
Jonah Emmanuel <jonahemma12@gmail.com>
2022-12-20 22:23:38 +0100

    update

--- lib/add_transaction.dart
+++ lib/add_transaction.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class AddTranscation extends StatefulWidget {
   const AddTranscation({super.key});
@@ -8,6 +9,9 @@ class AddTranscation extends StatefulWidget {
 }
 
 class _AddTranscationState extends State<AddTranscation> {
+  String credit = 'Income';
+  String out = 'Expense';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,39 +26,122 @@ class _AddTranscationState extends State<AddTranscation> {
         const SizedBox(
           height: 20,
         ),
+        const Text('Amount'),
         Row(
           children: [
             Container(
-              decoration: BoxDecoration(
-                  color: Colors.pink, borderRadius: BorderRadius.circular(16)),
-              padding: const EdgeInsets.all(18),
-              child: const Icon(
-                Icons.attach_money_outlined,
-                size: 24,
-                color: Colors.white,
-              ),
-            ),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16)),
+                padding: const EdgeInsets.all(18),
+                child: const Text(
+                  'NGN',
+                  style: TextStyle(color: Colors.pink, fontSize: 24),
+                )),
             const SizedBox(
               width: 12,
             ),
             Expanded(
                 child: TextField(
-                    decoration: InputDecoration(
-                        hintText: 'Amount', border: InputBorder.none),
-                    style: TextStyle(fontSize: 24),
-                    onChanged: (val) {
-                      try {
-                        int.parse(val);
-                      } catch (e) {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                            backgroundColor: Colors.white,
-                            content: Row(
-                              children: [],
-                            )));
-                      }
-                    }))
+              decoration: const InputDecoration(
+                  hintText: 'Amount',
+                  border: UnderlineInputBorder(
+                      borderSide: BorderSide(style: BorderStyle.solid))),
+              style: const TextStyle(fontSize: 24),
+              onChanged: (val) {
+                try {
+                  int.parse(val);
+                } catch (e) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      backgroundColor: Colors.white,
+                      duration: const Duration(seconds: 3),
+                      content: Row(
+                        children: const [
+                          Icon(
+                            Icons.info_outline_rounded,
+                            color: Colors.red,
+                          ),
+                          SizedBox(
+                            width: 12,
+                          ),
+                          Text(
+                            'Enter the Amount \n Thanks',
+                            style: TextStyle(fontSize: 24),
+                          )
+                        ],
+                      )));
+                }
+              },
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              keyboardType: TextInputType.number,
+            ))
           ],
-        )
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        const Text('Notes'),
+        const Expanded(
+            child: TextField(
+          decoration: InputDecoration(
+              hintText: 'Enter your Note',
+              border: UnderlineInputBorder(
+                  borderSide: BorderSide(style: BorderStyle.solid))),
+          style: TextStyle(fontSize: 24),
+        )),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ChoiceChip(
+              label: Text(
+                "Income",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: credit == "Income" ? Colors.white : Colors.black,
+                ),
+              ),
+              selectedColor: Colors.pink,
+              onSelected: (val) {
+                if (val) {
+                  setState(() {
+                    credit = "Income";
+                    if (out.isEmpty || out == "Expense") {
+                      out = 'Income';
+                    }
+                  });
+                }
+              },
+              selected: credit == "Income" ? true : false,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            ChoiceChip(
+              label: Text(
+                "Expense",
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: out == "Expense" ? Colors.white : Colors.black,
+                ),
+              ),
+              selectedColor: Colors.pink,
+              onSelected: (val) {
+                if (val) {
+                  setState(() {
+                    out = "Expense";
+                    if (out.isEmpty || out == "Expense") {
+                      credit = 'Expense';
+                    }
+                  });
+                }
+              },
+              selected: credit == "Expense" ? true : false,
+            ),
+          ],
+        ),
       ]),
     );
   }
