7afd2492ea30f4ed6cff2c42ba065ea0b1e27389
Jonah Emmanuel <jonahemma12@gmail.com>
2023-01-24 23:07:39 +0100

    update

--- lib/signUp.dart
+++ lib/signUp.dart
@@ -5,6 +5,28 @@ class SignUpPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          leading: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: const [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  "Let's get your business \n working",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.w700),
+                ),
+                
+              ),
+            ],
+          ),
+        ));
   }
 }
