0602d928f95509d463d64517abd84d30c4025c54
Jonah Emmanuel <jonahemma12@gmail.com>
2023-01-25 02:01:54 +0100

    update

--- lib/signUp.dart
+++ lib/signUp.dart
@@ -7,7 +7,7 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       resizeToAvoidBottomInset : false,
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         leading: const Icon(
